e43821dc513be3035d750a25680c287343d4548a
raphael correa <raphael.correa@bennu.com.br>
2020-04-03 19:53:05 -0300

    Updates

---
+++ precache-manifest.b98d0b02e31e4f70274f30148499fc42.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13745d10377c24376d28f181fa8a1530",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2da759f834fbc6dce3c6",
+    "url": "/static/css/main.abebfa88.chunk.css"
+  },
+  {
+    "revision": "8d48a01af3bdb174f012",
+    "url": "/static/js/2.2f88e27f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2f88e27f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2da759f834fbc6dce3c6",
+    "url": "/static/js/main.05a4b7b3.chunk.js"
+  },
+  {
+    "revision": "37590d19f67ccd104960",
+    "url": "/static/js/runtime-main.8e4e723c.js"
+  },
+  {
+    "revision": "991518aa0709c82c1990c103ac8e93a8",
+    "url": "/static/media/box.991518aa.svg"
+  },
+  {
+    "revision": "574dd35bd58632c8d74a5e1e9be23d3c",
+    "url": "/static/media/logo.574dd35b.svg"
+  },
+  {
+    "revision": "29b6bd64c1c1268c9a6048051e013831",
+    "url": "/static/media/tape-measure.29b6bd64.svg"
+  }
+]);
